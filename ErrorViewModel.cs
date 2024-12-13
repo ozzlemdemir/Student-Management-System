519cf7233418d67959e61b70bb489b35e4b1873a
Özlem Demir <demirozlem700@gmail.com>
2024-12-13 13:18:50 +0300

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace okulYonetimSistemi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
